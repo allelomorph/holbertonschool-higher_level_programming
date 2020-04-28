664c2a1c276d897558adc902172538deb4e8969c
Samuel Pomeroy <1690@holbertonschool.com>
2020-04-28 21:06:42 +0000

    changed to preserving sign of last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ number = random.randint(-10000, 10000)
 
 if number < 0:  # Python returns different modulo than C for negative ints
     abs = number * -1
+    digit = (abs % 10) * -1
 else:
-    abs = number
+    digit = number %10
 
-digit = abs % 10
 print("Last digit of {:d} is {:d}".format(number, digit), end=" ")
 if digit > 5:
     print("and is greater than 5")
