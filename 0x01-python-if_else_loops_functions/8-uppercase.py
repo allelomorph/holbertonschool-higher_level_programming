73f9ef8241ed01ba6c9630497df6b7e7d2f87c31
Samuel Pomeroy <1690@holbertonschool.com>
2020-04-29 00:17:39 +0000

    rebuilt using without using range()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in range(0, len(str)):
-        if ord(str[c]) >= ord('a') and ord(str[c]) <= ord('z'):
-            c_temp = chr(ord(str[c]) - ord(' '))
-        else:
-            c_temp = str[c]
-        print(c_temp, end="")
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            c = chr(ord(c) - ord(' '))
+
+        print("{}".format(c), end="")
     print("")
