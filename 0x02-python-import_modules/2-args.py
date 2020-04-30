b01fd692f6e143d3dd69757f713d83b89f095873
Samuel Pomeroy <1690@holbertonschool.com>
2020-04-30 21:39:57 +0000

    first attempt t2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,19 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+
+    print(ac, end=" ")
+
+    if ac == 1:
+        print("argument", end = "")
+    else:
+        print("arguments", end = "")
+
+    if ac == 0:
+        print(".")
+    else:
+        print(":")
+
+    for i in range(1, ac + 1):
+        print("{:d}: {}".format(i, argv[i]))
