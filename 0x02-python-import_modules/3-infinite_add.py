91fd4c352bc64fe257bc804ff6ac83b4b24310a4
Samuel Pomeroy <1690@holbertonschool.com>
2020-04-30 22:04:09 +0000

    first attempt t3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,24 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+
+#    print(ac, end=" ")
+#    if ac == 1:
+#        print("argument", end="")
+#    else:
+#        print("arguments", end="")
+
+#    if ac == 0:
+#        print(".")
+#    else:
+#        print(":")
+
+#    for i in range(1, ac + 1):
+#        print("{:d}: {:d}".format(i, int(argv[i])))
+
+    sum = 0
+    for i in range(1, ac + 1):
+        sum += int(argv[i])
+
+    print("{:d}".format(sum))
