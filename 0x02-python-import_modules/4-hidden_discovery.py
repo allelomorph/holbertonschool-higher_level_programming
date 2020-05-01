58c95150e0b1629aa58c6623429545f3f3ee1eac
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-01 00:31:59 +0000

    removed comments for t3, first attempt t4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    defs = dir(hidden_4)
+    for i in range(0, len(defs)):
+        if defs[i][0] != '_' and defs[i][1] != '_':
+            print(defs[i])
