8715e736db6e598a27f5ae2745a005c0a8daf701
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 04:01:43 +0000

    first attempt t10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    truth_list = my_list[:]
+    for i, num in enumerate(my_list):
+        if num % 2 == 0:
+            truth_list[i] = True
+        else:
+            truth_list[i] = False
+    return truth_list
