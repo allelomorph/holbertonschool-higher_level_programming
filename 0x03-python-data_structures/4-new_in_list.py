879c6dc88c66069d97e1883218457b5811bc7bc6
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 01:23:10 +0000

    changed None error returns to my_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     elif idx > len(my_list):
-        return None
+        return my_list
     else:
         new_list = my_list[:]
         new_list[idx] = element
