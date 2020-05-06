0fa0c5fdec2f1d8c5bc69b42e7d71c302b669b45
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 05:21:01 +0000

    very inefficient but functional

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a):
+        a_1 = tuple_a[0]
+    else:
+        a_1 = 0
+
+    if len(tuple_b):
+        b_1 = tuple_b[0]
+    else:
+        b_1 = 0
+
+    if len(tuple_a) > 1:
+        a_2 = tuple_a[1]
+    else:
+        a_2 = 0
+
+    if len(tuple_b) > 1:
+        b_2 = tuple_b[1]
+    else:
+        b_2 = 0
+
+    return (a_1 + b_1, a_2 + b_2)
