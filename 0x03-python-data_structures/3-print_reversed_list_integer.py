082d94ed1e14bb823f748e43a62e9dfc3b47f5dd
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 01:13:09 +0000

    t3: changed to reversed() t4: added checks on len(my_list)

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
+    for i in reversed(range(0, len(my_list))):
         print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    new_list[idx] = element
-    return new_list
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
