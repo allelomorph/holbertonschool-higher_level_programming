ea522d0f594ec2e9762e4a5d919e75f954b59fb3
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 04:36:51 +0000

    first attempts t8 t9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence:
+        first_c = sentence[0]
+    else:
+        first_c = None
+    return (length, first_c)
+
+"""    retvals = [len(sentence)]
+    if retvals[0] == 0:
+        retvals += 
+        else:
+            retvals += sentence[0]
+    return tuple(retvals)
+  #  return retvals
+"""
