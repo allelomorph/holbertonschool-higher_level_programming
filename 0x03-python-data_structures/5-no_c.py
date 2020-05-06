e5f53265220458a6ff8c21648515915fa15450d1
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 02:10:53 +0000

    first attempt t5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            new_string += i
+    return new_string
