2e5fc9cc760056893cf8e369518390f3e0a34731
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 01:25:00 +0000

    changed len check to >=

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
         new_list = my_list[:]
