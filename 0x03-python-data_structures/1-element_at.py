de6874ebd0d7050271ce5e8b8cd02eccdd9836a7
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 00:43:11 +0000

    removed None checks, changed len check to >=

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list is None:
+    if (idx < 0):
         return None
-    elif (idx < 0):
-        return None
-    elif (idx > len(my_list)):
+    elif (idx >= len(my_list)):
         return None
     else:
         return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if my_list is None or element is None:
-        return None
-    else:
         if (idx < 0):
             return my_list
-        elif (idx > len(my_list)):
+        elif (idx >= len(my_list)):
             return my_list
         else:
             my_list[idx] = element
