0f2f760297eda8b9cee39c19c02423889ec528c6
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 00:39:52 +0000

    added None checks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx < 0):
+    if my_list is None:
+        return None
+    elif (idx < 0):
         return None
     elif (idx > len(my_list)):
         return None
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (idx < 0):
-        return my_list
-    elif (idx > len(my_list)):
-        return my_list
+    if my_list is None or element is None:
+        return None
     else:
-        my_list[idx] = element
-        return my_list
+        if (idx < 0):
+            return my_list
+        elif (idx > len(my_list)):
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
