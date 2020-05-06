1e15116bee80224d5ffd5194378d4ce5e18f4cdc
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-06 03:16:42 +0000

    t6 first local success

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for i, elem in enumerate(row):
+                print("{:d}".format(row[i]), end="")
+                if i != len(row) - 1:
+                    print(" ", end="")
+                else:
+                    print()
+""" end="")
+                if num < (len(matrix[]) - 1):
+                      print(" ")
+                else:
+                      print()
+"""
+"""
+end=" " if num != (len(elem) - 1)
+            for j in range(0, len(matrix[i])):
+                if j < len(matrix[i] - 1):
+                    print("{:d}".format(matrix[i][j], end=" "))
+                else:
+                    print("{:d}".format(matrix[i][j]))
+"""
