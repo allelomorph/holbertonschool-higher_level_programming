bbce470c7ed013088eb0b4486ebc87792b6f9a6d
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-07 20:44:11 +0000

    first attempt t0 t1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([x ** 2 for x in row])
+    return new_matrix
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
