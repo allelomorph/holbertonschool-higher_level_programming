264ab4fdfe59a80b03b4e874203f352a419c6de5
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-07 21:38:25 +0000

    changed t3 to bitwise set op, first try t4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    shared = []
-    for a in set_1:
-        for b in set_2:
-            if a == b:
-                shared.append(a)
-    return set(shared)
+    return set_1 & set_2
