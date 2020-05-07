21138eb1e1b1da7ac21a02a01c089ca44225076c
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-07 21:26:01 +0000

    removal of redundancies with set()

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    temp = my_list[:]
+    temp = set(my_list)
     sum = 0
-    for x in temp:
-        if temp.count(x) > 1:
-            temp.remove(x)
     for x in temp:
         sum += x
     return sum
