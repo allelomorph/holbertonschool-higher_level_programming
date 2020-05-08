c832272a9b805039fec6c1506909b13f53cef9cf
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-08 06:33:33 +0000

    changed to .keys instead of .items

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for item in a_dictionary.items():
-        if item[1] == value:
-            del a_dictionary[item[0]]
-            break
+    for item in a_dictionary.keys():
+        if item == value:
+            del a_dictionary[item]
     return a_dictionary
