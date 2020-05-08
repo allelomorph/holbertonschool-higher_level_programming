0e3f3a61f1ce5fbf59f07bd819eedbf93af6f459
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-08 06:37:18 +0000

    copied dict to list to elininate conflict with iteration

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for item in a_dictionary.keys():
-        if item == value:
-            del a_dictionary[item]
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
     return a_dictionary
