bb319dbc2dd7471d58212af03cab488be73aa61a
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-08 05:28:17 +0000

    pep8 fixes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,9 @@
 def roman_to_int(roman_string):
     sum = 0
     last = 0
-    numerals = ( ('I', 1), ('V', 5), ('X', 10),
-                 ('L', 50), ('C', 100), ('D', 500),
-                 ('M', 1000,))
+    numerals = (('I', 1), ('V', 5), ('X', 10),
+                ('L', 50), ('C', 100), ('D', 500),
+                ('M', 1000,))
 
     if type(roman_string) is not str:
         return 0
