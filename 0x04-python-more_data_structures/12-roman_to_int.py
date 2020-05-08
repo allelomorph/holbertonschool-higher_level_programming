5f1335e09c86fd3c7c2a36ec986c509a291f6b73
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-08 05:29:47 +0000

    one last typo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     last = 0
     numerals = (('I', 1), ('V', 5), ('X', 10),
                 ('L', 50), ('C', 100), ('D', 500),
-                ('M', 1000,))
+                ('M', 1000))
 
     if type(roman_string) is not str:
         return 0
