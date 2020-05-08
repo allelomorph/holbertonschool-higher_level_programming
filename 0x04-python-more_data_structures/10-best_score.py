c48a1a6a4b77194d274e85c2827971187f98a2df
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-08 02:03:36 +0000

    first attempts t9 and t10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        temp = a_dictionary.items()
+        sort = (sorted(temp, key=lambda item: item[1], reverse=True))
+        return sort[0][0]
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for key in new_dict.keys():
+        new_dict[key] = a_dictionary[key] * 2
+    return new_dict
