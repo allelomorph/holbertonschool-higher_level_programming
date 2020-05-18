119760c69387869dc051e2af802061a9ee17ab09
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-18 23:44:07 +0000

    mistakenly thought retval needed to be int, removed conversion

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,4 @@ def safe_print_division(a, b):
         quot = None
     finally:
         print("Inside result: {}".format(quot))
-        if quot is not None:
-            quot = int(quot)
         return quot
