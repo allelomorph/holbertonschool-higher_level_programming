8965389d706d8340c395f9582c012ba6cb4bf748
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-18 22:01:27 +0000

    pep8 and changed to printing newline on 0 members printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i  in range (0, x):
+        for i in range(0, x):
             print(my_list[i], end="")
             count += 1
     except (IndexError, TypeError):
         pass
-    if count > 0:
-        print()
+    print()
     return count
