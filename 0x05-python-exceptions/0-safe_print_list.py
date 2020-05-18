16c131a1914b2154922f331239eae5611b982d2d
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-18 21:42:07 +0000

    first attempt t0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i  in range (0, x):
+            print(my_list[i], end="")
+            count += 1
+    except (IndexError, TypeError):
+        pass
+    if count > 0:
+        print()
+    return count
