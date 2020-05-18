2cf0d7102dc09835879f3909ef587d91865fc25a
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-18 23:38:09 +0000

    first attempt t3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        quot = a / b
+    except (ZeroDivisionError):
+        quot = None
+    finally:
+        print("Inside result: {}".format(quot))
+        if quot is not None:
+            quot = int(quot)
+        return quot
