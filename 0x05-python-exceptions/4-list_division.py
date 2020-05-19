aed33cafbcd77db7161ac8623505ada054952602
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-19 02:19:26 +0000

    first attempt t4, empty finally clause to satisfy project requirements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    quot = []
+    for i in range(0, list_length):
+        try:
+            quot.append(my_list_1[i] / my_list_2[i])
+        except (TypeError):
+            print("wrong type")
+            quot.append(0)
+        except (ZeroDivisionError):
+            print("division by 0")
+            quot.append(0)
+        except (IndexError):
+            print("out of range")
+            quot.append(0)
+        finally:
+            pass
+    return quot
