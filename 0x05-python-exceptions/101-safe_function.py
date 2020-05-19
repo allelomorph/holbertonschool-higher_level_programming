277bc2ff770e84362fefe4f6c993c45f4783e3ac
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-19 03:21:34 +0000

    first attempt t101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+
+    try:
+        retval = fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    else:
+        return retval
