36b44e0f941e90759e58eef17165a22d7710c688
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-19 03:13:17 +0000

    first attempt t100 with specified exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
+
+# 'except Exception as err:' for all exceptions
