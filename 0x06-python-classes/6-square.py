f7a643e445d2495cfa4d8794d717ba66aa3fca1f
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-20 03:47:42 +0000

    first attempt t6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,2 +1,122 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+
+class Square:
+    """Class defined for square generation.
+
+    Args:
+        size (int): length of one side of square
+        position (tuple) ((int), (int)): horizontal offset in spaces,
+        vertical offset in newlines
+
+
+    Attributes:
+        __size (int): length of one side of square
+        __position (tuple) ((int), (int)): horizontal offset in spaces,
+        vertical offset in newlines
+
+
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """__size getter, setter with same method name
+
+        Returns:
+            __size (int): length of one side, squared
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Args:
+            value (int): length of one side of square
+
+        Attributes:
+            __size (int): length of one side of square
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """__position getter, setter with same method name
+
+        Returns:
+            __position (tuple) ((int), (int)): horizontal offset in spaces,
+            vertical offset in newlines
+
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Args:
+            value (int): tuple of two positive integers
+
+        Attributes:
+            __position (tuple) ((int), (int)): horizontal offset in spaces,
+            vertical offset in newlines
+
+        Raises:
+            TypeError: if value is not a tuple of two positive ints
+
+        """
+        if type(value) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        for num in value:
+            if type(num) is not int or num < 0:
+                raise TypeError('position must be a tuple of',
+                                '2 positive integers')
+        self.__position = value
+        print("{} is __position after setting.".format(__position))
+
+    def area(self):
+        """Calulates area of square.
+
+        Attributes:
+            __size (int): length of one side of square
+
+        Returns:
+            area (int): length of one side, squared
+
+        """
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        """Prints text representation of square in hash chars,
+        horizontally and vertically offset by first and second int
+        in __position, respectively.
+
+        Attributes:
+            __size (int): length of one side of square
+            __position (tuple) ((int), (int)): horizontal offset in spaces,
+            vertical offset in newlines
+
+        """
+        for v_offset in range(0, self.__position[1]):
+            print()
+        for row in range(0, self.__size):
+            for h_offset in range(0, self.__position[0]):
+                print(" ", end="")
+            for col in range(0, self.__size):
+                print("#", end="")
+            print()
+        if self.__size is 0:
+            print()
