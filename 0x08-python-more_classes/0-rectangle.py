1cf9f4db1b42cdd125a2c3030daaa853267521ea
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-24 02:54:30 +0000

    first push with t0 and empty files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""0-rectangle, built for Hoblerton project 0x08 task 0
+"""
+
+class Rectangle:
+    """Empty class per task intructions, will be built upon in later tasks.
+    """
+    pass
