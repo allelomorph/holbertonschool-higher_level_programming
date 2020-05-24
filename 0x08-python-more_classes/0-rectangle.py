02f3eed5bd1fa7b99c5181c91defd969531a330a
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-24 03:19:27 +0000

    pep8 t0 and first attempt t1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,6 +2,7 @@
 """0-rectangle, built for Holberton project 0x08 task 0.
 """
 
+
 class Rectangle:
     """Empty class per task instructions, will be built upon in later tasks.
     """
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""1-rectangle, built for Holberton Python project 0x08 task 1.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = height
