376347ad822b22ee3c8571c418bbe639cbf22c69
Samuel Pomeroy <1690@holbertonschool.com>
2020-06-03 05:07:27 +0000

    updating integer_validator to reflect latest version of t7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,10 +25,11 @@ class BaseGeometry:
             ValueError: if `value` is less than or equal to 0
 
         """
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+        if name != "":
+            if type(value) != int:
+                raise TypeError('{} must be an integer'.format(name))
+            if value <= 0:
+                raise ValueError('{} must be greater than 0'.format(name))
 
 
 class Rectangle(BaseGeometry):
