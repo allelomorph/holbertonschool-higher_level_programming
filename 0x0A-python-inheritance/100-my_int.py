4faadf6453bb920f039cd86dfa2bdbbef33307f8
Samuel Pomeroy <1690@holbertonschool.com>
2020-06-03 06:14:27 +0000

    first attempt t12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" 0x0A. Python - Inheritance, task 12 """
+
+
+class MyInt(int):
+    """Custom int type inverting behavior of != and == operators.
+
+    """
+
+    def __eq__(self, other):
+        """Reverses behavior of == operator.
+
+        """
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+        """Reverses behavior of != operator.
+
+        """
+        return int(self) == int(other)
