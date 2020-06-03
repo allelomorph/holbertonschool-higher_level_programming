e9d533811c0b17b70e5d02b2e62e587d88f9b3b5
Samuel Pomeroy <1690@holbertonschool.com>
2020-06-03 19:54:49 +0000

    first push with empty files and first attempt t0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""0x0B. Python - Input/Output, task 0. Read file """
+
+def read_file(filename=""):
+    """Reads contents of a text file and print to stdout.
+
+    Args:
+        filename (str): name of file to be opened
+
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
