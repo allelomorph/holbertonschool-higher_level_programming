d53bf36105402b225cd456a6533465a0ee51cb1d
Samuel Pomeroy <1690@holbertonschool.com>
2020-06-05 17:46:05 +0000

    changed init of OD to match line counts in example Traceback

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,30 +8,21 @@ if __name__ == '__main__':
 
     line_no = 0
     total_file_size = 0
-
-    code_counts = OrderedDict()
-    code_counts['200'] = 0
-    code_counts['301'] = 0
-    code_counts['400'] = 0
-    code_counts['401'] = 0
-    code_counts['403'] = 0
-    code_counts['404'] = 0
-    code_counts['405'] = 0
-    code_counts['500'] = 0
+    code_counts = OrderedDict.fromkeys([200, 301, 400, 401, 403,
+                                        404, 405, 500], 0)
 
     try:
-        while True:
-            for line in sys.stdin:
-                line_no += 1
-                tokens = line.split(' ')
-                code = tokens[-2]
-                code_counts[code] += 1
-                total_file_size += int(tokens[-1])
-                if line_no % 10 == 0:
-                    print("File size: {}".format(total_file_size))
-                    for code in code_counts:
-                        if code_counts[code] > 0:
-                            print("{}: {}".format(code, code_counts[code]))
+        for line in sys.stdin:
+            line_no += 1
+            tokens = line.split(' ')
+            code = int(tokens[-2])
+            code_counts[code] += 1
+            total_file_size += int(tokens[-1])
+            if line_no % 10 == 0:
+                print("File size: {}".format(total_file_size))
+                for code in code_counts:
+                    if code_counts[code] > 0:
+                        print("{}: {}".format(code, code_counts[code]))
 
     except KeyboardInterrupt:
         print("File size: {}".format(total_file_size))
