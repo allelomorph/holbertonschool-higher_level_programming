049c4949164f2d7b6830c4b9d828323fd41cfd52
Samuel Pomeroy <1690@holbertonschool.com>
2020-06-05 17:11:01 +0000

    removed creation of log file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,12 +4,7 @@
 
 if __name__ == '__main__':
     import sys
-    import os
     from collections import OrderedDict
-
-    path = '101-log.txt'
-    if os.path.exists(path):
-        os.remove(path)
 
     line_no = 0
     total_file_size = 0
@@ -26,24 +21,21 @@ if __name__ == '__main__':
 
     try:
         while True:
-            with open(path, 'a', encoding='utf-8') as log:
-                for line in sys.stdin:
-                    line_no += 1
-                    log.write(line)
-                    tokens = line.split(' ')
-                    code = tokens[-2]
-                    code_counts[code] += 1
-                    total_file_size += int(tokens[-1])
-                    if line_no % 10 == 0:
-                        print("File size: {}".format(total_file_size))
-                        for code in code_counts:
-                            if code_counts[code] > 0:
-                                print("{}: {}".format(code, code_counts[code]))
+            for line in sys.stdin:
+                line_no += 1
+                tokens = line.split(' ')
+                code = tokens[-2]
+                code_counts[code] += 1
+                total_file_size += int(tokens[-1])
+                if line_no % 10 == 0:
+                    print("File size: {}".format(total_file_size))
+                    for code in code_counts:
+                        if code_counts[code] > 0:
+                            print("{}: {}".format(code, code_counts[code]))
 
     except KeyboardInterrupt:
         print("File size: {}".format(total_file_size))
         for code in code_counts:
             if code_counts[code] > 0:
                 print("{}: {}".format(code, code_counts[code]))
-        log.close()
         raise
