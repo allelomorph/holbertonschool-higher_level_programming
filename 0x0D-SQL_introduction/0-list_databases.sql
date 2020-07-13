dca691d3c6ac24f8165cb71df54ffef4f349f461
Samuel Pomeroy <1690@holbertonschool.com>
2020-07-13 23:36:47 +0000

    changed comment syntax to match checker's standard

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
-/* 0x0D. SQL - Introduction, task 0. List databases */
-# Shows all current databases. Synonymous with SHOW SCHEMAS;.
+-- 0x0D. SQL - Introduction, task 0. List databases
+-- Shows all current databases. Synonymous with SHOW SCHEMAS;.
 SHOW DATABASES;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
-/* 0x0D. SQL - Introduction, task 7. First add */
-# Inserts new row into `first_table`, DB passed as script arg.
+-- 0x0D. SQL - Introduction, task 7. First add
+-- Inserts new row into `first_table`, DB passed as script arg.
 INSERT INTO first_table (id, name)
        VALUES (89, 'Holberton School');
