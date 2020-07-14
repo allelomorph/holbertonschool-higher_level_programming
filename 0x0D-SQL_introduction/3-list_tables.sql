ee673c29667c20cf7f2fb51cac81e6d0e771c7dc
Samuel Pomeroy <1690@holbertonschool.com>
2020-07-14 22:35:40 +0000

    comment syntax t0-8

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
-/* 0x0D. SQL - Introduction, task 3. List tables */
-# Shows all tables in a database given as cmd line arg to mysql.
+-- 0x0D. SQL - Introduction, task 3. List tables
+-- Shows all tables in a database given as cmd line arg to mysql.
 SHOW TABLES;
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
-/* 0x0D. SQL - Introduction, task 6. List all in table */
-# Prints all populated fields of `first_table`, DB passed as script arg.
+-- 0x0D. SQL - Introduction, task 6. List all in table
+-- Prints all populated fields of `first_table`, DB passed as script arg.
 SELECT * FROM first_table;
