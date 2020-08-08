a6a7add41e0f48043362547ad8803d1ac06a440f
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-08 21:08:27 +0000

    first attempt t1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) != 4:
+        print('Use: 0-select_states.py <mysql username> <mysql password>'
+              ' <database name>')
+        sys.exit()
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
