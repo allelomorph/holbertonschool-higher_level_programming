14d960f47ca4d489823b4608ecfb8eff6b0a7fb3
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-08 20:43:26 +0000

    first push with empty files; first attempt t0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 0. Get all states"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) != 4:
+        print ('Use: 0-select_states.py <mysql username> <mysql password>'
+               ' <database name>')
+        sys.exit()
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
