fbaca5cfd6406cd131f1d1af12fe295a574d75ce
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-09 01:28:01 +0000

    added required use of `format`

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '" + sys.argv[4] +
-                "' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY "
+                "'{}' ORDER BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
