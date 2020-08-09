27550075e454c2661e7dee229e78f4caf9d51adf
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-09 23:46:27 +0000

    first attempt t11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 11. Add a new state
+"""
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    if len(argv) != 4:
+        exit('Use: 11-model_state_insert.py <mysql username> '
+             '<mysql password> <database name> ')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)  # creates decprecated warning
+    session = Session(engine)
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)  # 'pending', not added to db yet
+    session.commit()
+
+    print(new_state.id)
+    session.close()
