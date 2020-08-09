6b587d71d0b5f5f39055a614e85656a0e81aa4b2
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-09 08:27:48 +0000

    first attempt t8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 8. First state
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    if len(argv) != 4:
+        sys.exit('Use: 8-model_state_fetch_first.py <mysql username> '
+                 '<mysql password> <database name>')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+    session.close()
