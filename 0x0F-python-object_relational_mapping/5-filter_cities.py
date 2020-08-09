a1872179a155aae70b3f7dd2ffead633a0573cbb
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-09 02:36:44 +0000

    first attempt t5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 5. All cities by state"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) != 5:
+        sys.exit('Use: 5-filter_cities.py <mysql username> <mysql password>'
+                 ' <database name> <state name>')
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities LEFT JOIN states "
+                "ON cities.state_id = states.id WHERE states.name = %s "
+                "ORDER BY cities.id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
