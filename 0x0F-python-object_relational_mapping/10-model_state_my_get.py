beb88e3e2ab9758767dcb20cfa05091b945bc684
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-09 09:12:08 +0000

    first attempt t10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 10. Get a state
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    if len(argv) != 5:
+        sys.exit('Use: 10-model_state_my_get.py <mysql username> '
+                 '<mysql password> <database name> '
+                 '<state name to search>')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    try:
+        state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print(state.id)
+    else:
+        print('Not found')
+    session.close()
