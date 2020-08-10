c06e0376805ebc033e80625fe7913d795129201b
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-10 00:04:50 +0000

    first attempt t12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 12. Update a state
+"""
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    if len(argv) != 4:
+        exit('Use: 12-model_state_update_id_2.py <mysql username> '
+             '<mysql password> <database name> ')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    session = Session(engine)
+    Base.metadata.create_all(engine)  # creates decprecated warning
+
+    state_2 = session.query(State).filter_by(id=2).first()
+    if state_2 is not None:
+        state_2.name = 'New Mexico'
+    session.commit()
+    session.close()
