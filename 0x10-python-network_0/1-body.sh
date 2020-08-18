34e6ac13c85caa56c0d78849843914bcc5ce35de
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-18 20:09:30 +0000

    first attempt t1,2

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# follow redirect in 200 status code HTTP response and display content
+curl -L "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# send DELETE HTTP request and display response
+curl -X DELETE "$1"
