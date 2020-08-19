9aadb22d0c9385764efaa3082eee330f23cab2e2
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-19 09:07:30 +0000

    first attempt t17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""0x0F. Python - Object-relational mapping - task 17. From city
+"""
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from relationship_state import Base, State
+    from relationship_city import City
+
+    if len(argv) != 4:
+        exit('Use: 101-relationship_states_cities_list.py <mysql username> '
+             '<mysql password> <database name>')
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                           '{}'.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    session = Session(engine)
+    Base.metadata.create_all(engine)  # creates decprecated warning 1681
+
+    for city, state in session.query(City, State).filter(
+            City.state_id == State.id).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
+    session.close()
