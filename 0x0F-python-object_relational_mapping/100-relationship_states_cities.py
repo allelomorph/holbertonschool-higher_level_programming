078000d638b71df5ce2ecf1d653e8a1a46087b11
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-19 07:40:05 +0000

    changed from `add_all` to `add`

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
 
     new_state = State(name='California')
     new_city = City(name='San Francisco', state_id=new_state.id)
-    session.add_all([new_state, new_city])
+    session.add(new_state)
+    session.add(new_city)
     session.commit()
     session.close()
