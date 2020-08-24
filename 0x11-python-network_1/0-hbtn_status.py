ea573d43544240d9006e7476e6ffea68b76864a4
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-24 17:07:30 +0000

    first attempt t0 and empty files

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""0x11. Python - Network #1, task 0. What's my status? #0
+"""
+
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("""Body response:
+    \t- type: {}
+    \t- content: {}
+    \t- utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
