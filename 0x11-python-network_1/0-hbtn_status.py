8ac29cac63cdf26248fc59e3af7a602aea705c7a
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-24 20:11:21 +0000

    split into separate print statements to satisfy checker

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
 
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    print("""Body response:
-    \t- type: {}
-    \t- content: {}
-    \t- utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
     # charset can be gained with response.headers.get_content_charset()
