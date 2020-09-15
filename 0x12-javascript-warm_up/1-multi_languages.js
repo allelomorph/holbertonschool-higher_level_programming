e8cb9a5547659cf346b736ecc9535626dab52126
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-15 22:59:28 +0000

    first attempt t0-2; empty files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'Javascript is amazing';
+console.log(`${line1}\n${line2}\n${line3}`);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.length - 2;
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
