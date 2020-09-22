3ebd9a234e7248edd9a8ace47943db033742d6ae
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-22 06:26:00 +0000

    restore default arg syntax; include import of `5-square.js`

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 // create class `Square` which inherits from `Rectangle`; adds charPrint method
-const Rectangle = require('./4-rectangle');
+const SquareBase = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareBase {
   constructor (size) {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
+  charPrint (c = 'X') {
     for (let i = 0; i < this.width; i++) {
       console.log(c.repeat(this.width));
     }
