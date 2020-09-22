9db1c63dc132edcb2d2304cd8ed898ce890f346a
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-22 07:56:28 +0000

    replaced `indexOf()` with `Array.prototype.map()` 2nd arg

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,8 +3,8 @@
 const list = require('./100-data').list;
 
 console.log(list);
-if (Array.isArray(list) && list.every(n => typeof n === 'number')) {
-  const newList = list.map(n => n * list.indexOf(n));
+if (Array.isArray(list) && list.every(elem => typeof elem === 'number')) {
+  const newList = list.map((value, index) => value * index);
   console.log(newList);
 } else {
   console.log(undefined);
