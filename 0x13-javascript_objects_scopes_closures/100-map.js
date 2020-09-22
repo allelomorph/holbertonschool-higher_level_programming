fe01604692d5731836a5612f07634a1bccf5f9e0
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-22 07:17:47 +0000

    const declaration of `list`; changed to index as multiplier

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // imports array from data file; creates new array with map; prints both
-list = require('./100-data').list;
-const listDoubled = list.map(n => n * 2);
+const list = require('./100-data').list;
+
+const newList = list.map(n => n * list.indexOf(n));
 console.log(list);
-console.log(listDoubled);
+console.log(newList);
