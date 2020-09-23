b6cabd98900dd97f786ff0652822f929301f5cb5
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-23 02:52:13 +0000

    removed superflous toString()s, cloned edit of return dict

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,21 @@
 #!/usr/bin/node
-// computes the number of tasks completed by user id
+// totals number of tasks completed per user_id, omitting those with 0
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
   }
   const taskList = JSON.parse(body);
   const userTotals = {};
   for (const task of taskList) {
-    if (task.userId.toString() in userTotals) {
-      if (task.completed === true) {
-        userTotals[task.userId.toString()] += 1;
+    if (task.completed) {
+      if (task.userId in userTotals) {
+        userTotals[task.userId] += 1;
+      } else {
+        userTotals[task.userId] = 1;
       }
-    } else {
-      userTotals[task.userId.toString()] = 0;
     }
   }
-  const userTotalsEdit = { ...userTotals };
-  for (const key in userTotalsEdit) {
-    if (userTotalsEdit[key] === 0) {
-      delete userTotalsEdit[key];
-    }
-  }
-  console.log(userTotalsEdit);
+  console.log(userTotals);
 });
