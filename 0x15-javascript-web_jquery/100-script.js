9f441da2dbd365c0dd75a71df7359267f26b1cd7
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-25 11:41:42 +0000

    more standard success syntax t9; first attempts t10-12

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+// DOMContentLoaded is just after HTML loaded, vs `load`, which is all content
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
