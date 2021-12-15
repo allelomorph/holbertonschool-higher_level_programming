0a705ccbb9473d5d319cbfe2b605679368c91eca
Samuel Pomeroy <1690@holbertonschool.com>
2021-12-15 23:25:09 +0000

    0x04-0x07 added provided files

---
+++ 0x04-python-more_data_structures/tests/0-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
---
+++ 0x04-python-more_data_structures/tests/7-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+update_dictionary = __import__('7-update_dictionary').update_dictionary
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
