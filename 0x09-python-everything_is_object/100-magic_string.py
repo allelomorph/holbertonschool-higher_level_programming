4c4f12554a12f6485d62f14f522e0fc061a04005
Samuel Pomeroy <1690@holbertonschool.com>
2021-12-17 01:23:14 +0000

    0x09 t30 added alternate solution

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+
 def magic_string():
-    setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
-    return 'Holberton' + ', Holberton' * getattr(magic_string, 'rep', 0)
+    # function objects can have attributes, here at global count of calls
+    if hasattr(magic_string, 'calls'):
+        magic_string.calls += 1
+    else:
+        magic_string.calls = 1
+
+    return ', '.join(['Holberton'] * magic_string.calls)
+
+# Original solution
+#
+# #!/usr/bin/python3
+# def magic_string():
+#     setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
+#     return 'Holberton' + ', Holberton' * getattr(magic_string, 'rep', 0)
