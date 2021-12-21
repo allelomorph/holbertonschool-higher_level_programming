f9d1dbd0a689db1e8f48d5fc304e34a003b1d58d
Samuel Pomeroy <1690@holbertonschool.com>
2021-12-21 21:02:30 +0000

    0x12 t11 now sorting by value rather than characters

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,13 @@ const argvN = [];
 for (let i = 0; i < process.argv.length - 2; i++) {
   argvN[i] = Math.floor(Number(process.argv[i + 2]));
 }
+
 const filtered = argvN.filter(n => !isNaN(n));
-const sorted = filtered.sort();
+// default sort is alphabetic, see https://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly
+const sorted = filtered.sort(function (a, b) {
+  return a - b;
+});
+
 if (sorted.length < 2) {
   console.log(0);
 } else {
